0a6456305042c28e108a23a619a70b8464f64af8
Kenny Goh <goh.kar.hong@gmail.com>
2021-05-09 20:01:48 +1000

    added controller, service, model and tests

--- src/models/db.js
+++ src/models/db.js
@@ -2,17 +2,17 @@ const mongoose = require('mongoose');
 const { MongoMemoryServer } = require('mongodb-memory-server');
 const config = require('../config');
 
-async function connect() {
-  let uri = config.db.url;
-  if (config.environment === 'dev') {
-    const mongod = new MongoMemoryServer();
-    uri = await mongod.getUri();
-  }
-  const mongooseOpts = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  };
-  await mongoose.connect(uri, mongooseOpts);
-}
-// connect();
+// async function connect() {
+//   let uri = config.db.url;
+//   if (config.environment === 'dev') {
+//     const mongod = new MongoMemoryServer();
+//     uri = await mongod.getUri();
+//   }
+//   const mongooseOpts = {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   };
+//   await mongoose.connect(uri, mongooseOpts);
+// }
+
 module.exports = mongoose.connection;
