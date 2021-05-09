ee5e2ca0a4f83e39d133c4a2b7d2139a826b201b
Kenny Goh <goh.kar.hong@gmail.com>
2021-05-09 12:50:31 +1000

    test

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express 2' });
+  res.render('index', { title: 'Express 3' });
 });
 
 module.exports = router;
